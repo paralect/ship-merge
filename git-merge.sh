bc86df27f32482e044fee0c1d2e624a9591d0b9c
Evgeny Zhivitsa <evgeny.zhivitsa@gmail.com>
2018-02-01 23:05:33 +0300

    [+] Add commands to remove all-contributors

--- git-merge.sh
+++ git-merge.sh
@@ -17,40 +17,60 @@ environmentPaths=( "web/src/server/config/environment"
                    "api/src/config/environment"
                    "landing/src/server/config/environment" )
 
-filesToRemove=( ".drone.yml" "docker-compose.yml" "LICENSE" "CHANGELOG.md CODE_OF_CONDUCT.md" )
+filesToRemove=( ".drone.yml"
+                "docker-compose.yml"
+                "LICENSE"
+                "CHANGELOG.md"
+                "CODE_OF_CONDUCT.md"
+                ".all-contributorsrc" )
 
 repositoryActions() {
-    declare -a files=("${!3}")
-    cd ./$1
-    
-    echo "### $1 ###"
+  declare -a files=("${!3}")
+  cd ./$1
+  
+  echo "### $1 ###"
 
-    echo "=== CHECKOUT TO THE LATEST TAG ==="
-    tags=($(git tag))
+  echo "=== CHECKOUT TO THE LATEST TAG ==="
+  tags=($(git tag))
 
-    if [ ${#tags[@]} -gt 0 ]
-    then
-        latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
-        git checkout tags/${latestTag}
-    fi
-    echo "=== DONE CHECKOUT ==="
+  if [ ${#tags[@]} -gt 0 ]
+  then
+      latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+      git checkout tags/${latestTag}
+  fi
+  echo "=== DONE CHECKOUT ==="
 
-    echo "=== START REMOVE UNNECESSARY FILES FROM HISTORY ==="
-    
-    git filter-branch --tree-filter "GLOBIGNORE='n*'; rm ${files[*]}; mkdir -p ../temp_path; mv * ../temp_path; mkdir $2; mv ../temp_path/* $2/; unset GLOBIGNORE" --force HEAD
-    
-    git branch -D master
-    git checkout -b master
-    echo "=== DONE REMOVE FILES FROM HISTORY ==="
+  echo "=== START REMOVE UNNECESSARY FILES FROM HISTORY ==="
+  
+  git filter-branch --tree-filter "
+    GLOBIGNORE='n*';
+    rm ${files[*]};
+    mkdir -p ../temp_path;
+    mv * ../temp_path;
+    mkdir $2;
+    mv ../temp_path/* $2/;
+    unset GLOBIGNORE;
+  " --force HEAD
+  
+  git branch -D master
+  git checkout -b master
+  echo "=== DONE REMOVE FILES FROM HISTORY ==="
 
-    cd ../
+  cd ../
+}
+
+removeAllContributors() {
+  # Remove all contributors from package.json
+  sed -i -e '/all-contributor/d; :a;N;$!ba;s/,\n  }/\n  }/g' package.json
+  # Remove all contributors from README.md
+  sed -i '/All Contributors/d; /^## Contributors/,$d' README.md
 }
 
 echo "=== CLONE REPOSITORIES ==="
 for element in ${repositories[@]}
 do
-    echo "clonning $element"
-    git clone $element
+  echo "clonning $element"
+  git clone $element
 done
 echo "=== DONE CLONE REPOSITORIES ==="
 
@@ -63,9 +83,9 @@ cd ./ship
     
 for path in ${paths[@]}
 do
-    git remote add repo-$path ../$path/.git
-    git pull repo-$path master --allow-unrelated-histories --no-edit
-    git remote rm repo-$path
+  git remote add repo-$path ../$path/.git
+  git pull repo-$path master --allow-unrelated-histories --no-edit
+  git remote rm repo-$path
 done
 echo "=== END COPY COMMITS ==="
 cd ../../
@@ -73,6 +93,6 @@ cd ../../
 echo "=== COPY STAGING ENVIRONMENT FILE ==="
 for envPath in ${environmentPaths[@]}
 do
-    cp ./staging.js "./temp_repos/ship/$envPath/staging.js"
+  cp ./staging.js "./temp_repos/ship/$envPath/staging.js"
 done
 echo "=== DONE COPY STAGING ENVIRONMENT FILE ==="
